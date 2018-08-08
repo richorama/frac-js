364d5b7073b7da1aa174e4f4c671b0c9760cf24a
Richard Astbury <richard.astbury@gmail.com>
2018-08-08 15:06:08 +0100

    use hsl colour space

--- worker.js
+++ worker.js
@@ -25,10 +25,10 @@ function draw(x,y,z, data){
         value = getColour(x1 + (dx * pixelSize), y1 + (dy * pixelSize));
 
         if (value >= 0) {
-            var sinVal = Math.floor(255 * Math.sin(value * Math.PI/255))
-            data[i]     = sinVal;     // red
-            data[i + 1] = value; // green
-            data[i + 2] = 255 - sinVal; // blue
+			const rgb = hslToRgb(value / 255, 0.5, 0.5);
+            data[i]     = rgb[0];     // red
+            data[i + 1] = rgb[1]; // green
+            data[i + 2] = rgb[2]; // blue
             data[i + 3] = 255; // alpha
         } else {
             //data[i]     = 0;     // red
@@ -74,3 +74,30 @@ function getColour(re, im) {
 	}
 	return -1
 }
+
+function hslToRgb(h, s, l) {
+	var r, g, b;
+  
+	if (s == 0) {
+	  r = g = b = l; // achromatic
+	} else {
+	  function hue2rgb(p, q, t) {
+		if (t < 0) t += 1;
+		if (t > 1) t -= 1;
+		if (t < 1/6) return p + (q - p) * 6 * t;
+		if (t < 1/2) return q;
+		if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+		return p;
+	  }
+  
+	  var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+	  var p = 2 * l - q;
+  
+	  r = hue2rgb(p, q, h + 1/3);
+	  g = hue2rgb(p, q, h);
+	  b = hue2rgb(p, q, h - 1/3);
+	}
+  
+	return [ r * 255, g * 255, b * 255 ];
+  }
+  
