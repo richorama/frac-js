056d59cd4a91ff5ea4adba1696953e2bcac8e8b8
Richard Astbury <richard.astbury@gmail.com>
2018-08-21 16:25:30 +0100

    adjust according to micro benchmarking

--- worker.js
+++ worker.js
@@ -100,26 +100,28 @@ renderingFunctions.mandlebrot = (real, imag) => {
 	return -1
 }
 
+
+function hue2rgb(p, q, t) {
+	if (t < 0) t++;
+	if (t > 1) t--;
+	if (t < 1/6) return p + (q - p) * 6 * t;
+	if (t < 1/2) return q;
+	if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+	return p;
+}
+
 function hslToRgb(h, s, l) {
-	var r, g, b;
+	let r, g, b;
   
 	if (s == 0) {
-	  r = g = b = l; // achromatic
+		r = g = b = l; // achromatic
 	} else {
-	  function hue2rgb(p, q, t) {
-		if (t < 0) t += 1;
-		if (t > 1) t -= 1;
-		if (t < 1/6) return p + (q - p) * 6 * t;
-		if (t < 1/2) return q;
-		if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-		return p;
-	  }
-  
-	  var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-	  var p = 2 * l - q;
-  
-	  r = hue2rgb(p, q, h + 1/3);
-	  g = hue2rgb(p, q, h);
+
+		const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+		const p = 2 * l - q;
+
+		r = hue2rgb(p, q, h + 1/3);
+		g = hue2rgb(p, q, h);
 	  b = hue2rgb(p, q, h - 1/3);
 	}
   
